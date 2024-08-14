7e251b986caf804eabb3c86aceea7df65c274e44
Santiago Lombardi <santiagonicolaslombardi2005@gmail.com>
2024-08-13 21:45:13 -0300

    added assets

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -1,13 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { ColorBlueMid } from "../styles/constants";
+
+const image = {
+  uri: '../assets/Logo.png',
+  width: 55,
+  height: 25,
+};
+
 const Topbar = () => {
   return (
     <View
       style={{ backgroundColor: ColorBlueMid, padding: 20 }}
       className="p-2"
     >
-      <Text style={{ color: "#fff", fontSize: 16, fontWeight: "600" }}>JOBS</Text>
+      <Image source={image} />
+      {/* <Text style={{ color: "#fff", fontSize: 16, fontWeight: "600" }}>JOBS</Text> */}
     </View>
   );
 };
