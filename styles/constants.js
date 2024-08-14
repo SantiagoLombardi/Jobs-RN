80802c85290f014d657d587aad0f009e04f5cb1c
Santiago Lombardi <santiagonicolaslombardi2005@gmail.com>
2024-08-13 21:21:53 -0300

    created styles constants

---
+++ styles/constants.js
@@ -0,0 +1,17 @@
+const ColorBlueBG = "#1F202A";
+const ColorBlueDark = "#232530";
+const ColorBlueExtraDark = "#1D1E29";
+const ColorBlueMid = "#282A36";
+const ColorPink = "#FF79C6";
+const ColorLightBlue = "#8BE9FD";
+const ColorViolet = "#BE93F9";
+
+export {
+  ColorBlueBG,
+  ColorBlueMid,
+  ColorBlueDark,
+  ColorBlueExtraDark,
+  ColorPink,
+  ColorLightBlue,
+  ColorViolet,
+};
