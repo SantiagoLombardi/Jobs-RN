8238a087fcb6b369e616db603bc81327f777ff62
Santiago Lombardi <santiagonicolaslombardi2005@gmail.com>
2024-08-15 17:46:49 -0300

    added Logo asset to Topbar component

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -1,19 +1,16 @@
 import { View, Image } from "react-native";
 import { ColorBlueMid } from "../styles/constants";
 
-const image = {
-  uri: "../assets/images/Logo.png",
-  width: 55,
-  height: 25,
-};
-
 const Topbar = () => {
   return (
     <View
       style={{ backgroundColor: ColorBlueMid, padding: 20 }}
       className="p-2"
     >
-      <Image source={image} />
+      <Image
+        source={require("../assets/images/Logo.png")}
+        style={{ height: 20, width: 40 }}
+      />
       {/* <Text style={{ color: "#fff", fontSize: 16, fontWeight: "600" }}>JOBS</Text> */}
     </View>
   );
